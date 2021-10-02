b965a861eca1effa38884e9b2b725b579b269f6f
kolgie <82279897+kolgie@users.noreply.github.com>
2021-10-02 22:30:45 +0200

    Update standfarmtest-obfuscated.lua

--- standfarmtest-obfuscated.lua
+++ standfarmtest-obfuscated.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("Public test has ended. Please use the main script now.")
+game.Players.LocalPlayer:Kick("Public test has ended. It has merged with the main script, please use the main script now.")
